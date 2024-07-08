46b504b1c208c5837b8ed7859519a122a485f610
OMI786I <sohrawardy1998@gmail.com>
2024-07-08 14:40:04 +0600

    getting started

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,19 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./Components/Root.jsx";
+import Home from "./Components/Home.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div className="text-red-500">Hello world!</div>,
+    element: <Root></Root>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
   },
 ]);
 
