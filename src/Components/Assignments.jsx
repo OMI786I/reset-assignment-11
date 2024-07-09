599071dc25a7967747e24bb2e503c79db5808752
OMI786I <sohrawardy1998@gmail.com>
2024-07-09 19:34:23 +0600

    reading created assignment from mongodb

--- src/Components/Assignments.jsx
+++ src/Components/Assignments.jsx
@@ -1,12 +1,37 @@
-import { Toaster } from "react-hot-toast";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
 
 const Assignments = () => {
-  return (
-    <div>
-      <Toaster />
-      This is assignments page
-    </div>
-  );
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(true);
+  console.log(data);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/createdAssignment")
+      .then((assignment) => {
+        setData(assignment.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error(error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <span className="loading loading-spinner loading-lg"></span>
+      </div>
+    );
+  } else
+    return (
+      <div>
+        <Toaster />
+      </div>
+    );
 };
 
 export default Assignments;
