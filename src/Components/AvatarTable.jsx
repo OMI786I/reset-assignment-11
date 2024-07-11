8546a616424cdea52d30933551072c940294df71
OMI786I <sohrawardy1998@gmail.com>
2024-07-11 07:42:45 +0600

    getting started with client side

--- src/Components/AvatarTable.jsx
+++ src/Components/AvatarTable.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useReducer, useState } from "react";
 import { AuthContext } from "../Firebase/AuthProvider";
 import axios from "axios";
 
@@ -6,6 +6,7 @@ const AvatarTable = () => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const { user } = useContext(AuthContext);
+
   const email = user.email;
 
   useEffect(() => {
@@ -20,29 +21,35 @@ const AvatarTable = () => {
         console.log(error);
         setLoading(false);
       });
-  });
+  }, [email]);
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <span className="loading loading-spinner loading-lg"></span>
+      </div>
+    );
+  } else
+    return (
+      <div className="overflow-auto">
+        <table className="table table-xs">
+          <thead>
+            <th>Title</th>
+            <th>status</th>
 
-  return (
-    <div className="overflow-auto">
-      <table className="table table-xs">
-        <thead>
-          <th>Title</th>
-          <th>status</th>
-
-          <th>obtainedMarks</th>
-        </thead>
-        <tbody>
-          {data.map((data) => (
-            <tr key={data._id}>
-              <td>{data.title}</td>
-              <td>{data.status}</td>
-              <td> {data.obtainedMarks}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
+            <th>obtainedMarks</th>
+          </thead>
+          <tbody>
+            {data.map((data) => (
+              <tr key={data._id}>
+                <td>{data.title}</td>
+                <td>{data.status}</td>
+                <td> {data.obtainedMarks}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
 };
 
 export default AvatarTable;
--- src/Components/PendingAssignments.jsx
+++ src/Components/PendingAssignments.jsx
@@ -1,5 +1,65 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const PendingAssignments = () => {
-  return <div>This is pending assignments</div>;
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get("http://localhost:5000/submission?status=pending&sort=1")
+      .then((assignment) => {
+        setData(assignment.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+      });
+  }, []);
+  console.log(data);
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <span className="loading loading-spinner loading-lg"></span>
+      </div>
+    );
+  } else
+    return (
+      <div>
+        <h1 className="text-xl md:text-3xl my-10 text-center bg bg-green-500 rounded-xl text-white p-5">
+          Pending Assignments
+        </h1>
+        <div className="grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-10">
+          {data.map((data) => (
+            <div key={data._id}>
+              <div className="card bg-base-100 w-auto md:w-80 xl:w-96 shadow-xl">
+                <figure>
+                  <img src={data.photo} alt="Shoes" className="w-96 h-56" />
+                </figure>
+                <div className="card-body">
+                  <h2 className="card-title text-2xl">{data.title}</h2>
+                  <div className="flex items-center gap-3">
+                    <span className="font-bold">Total Marks</span>{" "}
+                    <p>{data.marks}</p>
+                  </div>
+                  <div className="flex gap-3">
+                    <span className="font-bold">Submitter</span>
+                    {data.submitterName}
+                  </div>
+                  <div>
+                    <button className="btn btn-success text-white">
+                      Give Mark
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
 };
 
 export default PendingAssignments;
