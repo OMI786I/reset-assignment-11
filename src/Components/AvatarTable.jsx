dc4a01f7789babf006144e6b796fa7d69f18bfa2
OMI786I <sohrawardy1998@gmail.com>
2024-07-12 18:28:17 +0600

    final

--- src/Components/AvatarTable.jsx
+++ src/Components/AvatarTable.jsx
@@ -9,7 +9,7 @@ const AvatarTable = () => {
 
   const email = user.email;
 
-  const fetchData = () => {
+  useEffect(() => {
     setLoading(true);
     axios
       .get(`http://localhost:5000/submission?submitterEmail=${email}&sort=1`, {
@@ -23,37 +23,36 @@ const AvatarTable = () => {
         console.log(error);
         setLoading(false);
       });
-  };
+  }, []);
 
-  useEffect(() => {
-    setLoading(true);
-    fetchData();
-    const intervalId = setInterval(fetchData, 1000); // Poll every 5 seconds
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <span className="loading loading-spinner loading-lg"></span>
+      </div>
+    );
+  } else
+    return (
+      <div className="overflow-auto">
+        <table className="table table-xs">
+          <thead>
+            <th>Title</th>
+            <th>status</th>
 
-    return () => clearInterval(intervalId); // Clean up the interval on component unmount
-  }, [email]);
-
-  return (
-    <div className="overflow-auto">
-      <table className="table table-xs">
-        <thead>
-          <th>Title</th>
-          <th>status</th>
-
-          <th>obtainedMarks</th>
-        </thead>
-        <tbody>
-          {data.map((data) => (
-            <tr key={data._id}>
-              <td>{data.title}</td>
-              <td>{data.status}</td>
-              <td> {data.obtainedMarks}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
+            <th>obtainedMarks</th>
+          </thead>
+          <tbody>
+            {data.map((data) => (
+              <tr key={data._id}>
+                <td>{data.title}</td>
+                <td>{data.status}</td>
+                <td> {data.obtainedMarks}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
 };
 
 export default AvatarTable;
