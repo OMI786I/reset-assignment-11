9ad1ad94cbea99d9906923f1cba07499bef6da68
OMI786I <sohrawardy1998@gmail.com>
2024-07-12 16:15:55 +0600

    added features section

--- src/Components/Faq.jsx
+++ src/Components/Faq.jsx
@@ -3,38 +3,51 @@ import { useEffect, useState } from "react";
 
 const Faq = () => {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    setLoading(true);
     axios
       .get(`/Questions.json`)
       .then((assignment) => {
+        setLoading(false);
         setData(assignment.data);
       })
       .catch((error) => {
+        setLoading(false);
         console.error(error);
       });
   }, []);
 
   console.log(data);
 
-  return (
-    <div className="border my-16 text-white rounded-xl p-5 bg-green-500">
-      <h1 className="text-center text-3xl my-10">Frequently Asked Questions</h1>
-      {data.map((dat) => (
-        <div className="join join-vertical w-full " key={data.question}>
-          <div className="collapse collapse-arrow join-item border-base-300 border ">
-            <input type="radio" name="my-accordion-4" defaultChecked />
-            <div className="collapse-title text-xl font-medium bg-[#ffbe00] text-black">
-              {dat.question}
-            </div>
-            <div className="collapse-content bg-base-200 text-black ">
-              <p>{dat.answer}</p>
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <span className="loading loading-spinner loading-lg"></span>
+      </div>
+    );
+  } else
+    return (
+      <div className="border my-16 text-white rounded-xl p-5 bg-green-600">
+        <h1 className="text-center text-3xl my-10">
+          Frequently Asked Questions
+        </h1>
+        {data.map((dat) => (
+          <div className="join join-vertical w-full " key={data.question}>
+            <div className="collapse collapse-arrow join-item border-base-300 border ">
+              <input type="radio" name="my-accordion-4" defaultChecked />
+              <div className="collapse-title text-xl font-medium bg-[#ffbe00] text-black">
+                {dat.question}
+              </div>
+              <div className="collapse-content bg-base-200 text-black ">
+                <p>{dat.answer}</p>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
-    </div>
-  );
+        ))}
+      </div>
+    );
 };
 
 export default Faq;
---
+++ src/Components/Features.jsx
@@ -0,0 +1,56 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const Features = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(`/Features.json`)
+      .then((assignment) => {
+        setLoading(false);
+        setData(assignment.data);
+      })
+      .catch((error) => {
+        setLoading(false);
+        console.error(error);
+      });
+  }, []);
+
+  console.log(data);
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <span className="loading loading-spinner loading-lg"></span>
+      </div>
+    );
+  } else
+    return (
+      <div className="my-20 bg-base-200 rounded-xl ">
+        <h1 className="text-3xl text-center my-10 bg-[#ffb300] p-8">
+          Features
+        </h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          {data.map((data) => (
+            <div
+              className="card bg-base-100 w-full  md:w-72 mx-auto my-6  shadow-xl pt-5"
+              key={data.icon}
+            >
+              <figure>
+                <img src={data.icon} className="w-16" alt="Shoes" />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">{data.title}</h2>
+                <p>{data.description}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+};
+
+export default Features;
